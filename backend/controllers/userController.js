dfadd2319c9d604658efb68bb6f1b84536250963
23016353-sherval <23016353@myrp.edu.sg>
2025-01-24 01:55:23 +0800

    Vid 7 done- loggin users in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,20 @@ const createToken = (_id) => {
 } 
 
 //login user
+
 const loginUser = async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        constuser = await User.login(email, password)
+
+        //create a token
+        const token = createToken(user._id)
+        
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
     res.json({msg: "login user"})
 }
 
